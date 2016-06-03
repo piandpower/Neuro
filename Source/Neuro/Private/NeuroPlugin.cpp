dd25441154e1ca12a53ad73c6ae00c2bba2b71e9
Ryan M. Shetley <ryan.shetley@gmail.com>
2016-06-03 18:13:23 -0500

    Updated uplugin, Added IModuleInterface methods

--- Source/Neuro/Private/NeuroPlugin.cpp
+++ Source/Neuro/Private/NeuroPlugin.cpp
@@ -4,4 +4,16 @@ DEFINE_LOG_CATEGORY(NeuroPlugin);
 
 class FNeuroPlugin : public INeuroPlugin {
 
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
 };
+
+IMPLEMENT_MODULE(FNeuroPlugin, NeuroPlugin);
+
+void FNeuroPlugin::StartupModule() {
+	UE_LOG(NeuroPlugin, Log, TEXT("Neuro: StartupModule()"));
+}
+void FNeuroPlugin::ShutdownModule() {
+	UE_LOG(NeuroPlugin, Log, TEXT("Neuro: ShutdownModule()"));
+}
